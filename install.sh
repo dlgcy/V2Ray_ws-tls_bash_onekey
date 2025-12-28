8b7037ecda9a04dd44dbdee059772c45798d8e46
dlgcy <dlgcywlh@qq.com>
2025-12-28 22:18:19 +0800

    尝试支持 almalinux

--- install.sh
+++ install.sh
@@ -75,9 +75,16 @@ source '/etc/os-release'
 VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
 
 check_system() {
-    if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
-        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
-        INS="yum"
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
+        local major_ver
+        major_ver=$(echo "${VERSION_ID}" | cut -d '.' -f1)
+        if [[ ${major_ver} -ge 7 ]]; then
+            echo -e "${OK} ${GreenBG} 当前系统为 ${ID^} ${VERSION_ID} ${VERSION} ${Font}"
+            INS="yum"
+        else
+            echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
+            exit 1
+        fi
     elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
         echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
         INS="apt"
@@ -133,7 +140,7 @@ chrony_install() {
 
     timedatectl set-ntp true
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         systemctl enable chronyd && systemctl restart chronyd
     else
         systemctl enable chrony && systemctl restart chrony
@@ -166,14 +173,14 @@ chrony_install() {
 dependency_install() {
     ${INS} install wget git lsof -y
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         ${INS} -y install crontabs
     else
         ${INS} -y install cron
     fi
     judge "安装 crontab"
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         touch /var/spool/cron/root && chmod 600 /var/spool/cron/root
         systemctl start crond && systemctl enable crond
     else
@@ -195,14 +202,14 @@ dependency_install() {
     ${INS} -y install curl
     judge "安装 curl"
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         ${INS} -y groupinstall "Development tools"
     else
         ${INS} -y install build-essential
     fi
     judge "编译工具包 安装"
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         ${INS} -y install pcre pcre-devel zlib-devel epel-release
     else
         ${INS} -y install libpcre3 libpcre3-dev zlib1g-dev dbus
@@ -216,7 +223,7 @@ dependency_install() {
 
     #    sed -i -r '/^HRNGDEVICE/d;/#HRNGDEVICE=\/dev\/null/a HRNGDEVICE=/dev/urandom' /etc/default/rng-tools
 
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         #       systemctl start rngd && systemctl enable rngd
         #       judge "rng-tools 启动"
         systemctl start haveged && systemctl enable haveged
@@ -239,7 +246,7 @@ basic_optimization() {
     echo '* hard nofile 65536' >>/etc/security/limits.conf
 
     # 关闭 Selinux
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
         setenforce 0
     fi
@@ -426,7 +433,7 @@ nginx_install() {
 }
 
 ssl_install() {
-    if [[ "${ID}" == "centos" ]]; then
+    if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
         ${INS} install socat nc -y
     else
         ${INS} install socat netcat -y
@@ -652,7 +659,7 @@ nginx_process_disabled() {
 acme_cron_update() {
     wget -N -P /usr/bin --no-check-certificate "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/dev/ssl_update.sh"
     if [[ $(crontab -l | grep -c "ssl_update.sh") -lt 1 ]]; then
-      if [[ "${ID}" == "centos" ]]; then
+      if [[ "${ID}" == "centos" || "${ID}" == "almalinux" ]]; then
           #        sed -i "/acme.sh/c 0 3 * * 0 \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
           #        &> /dev/null" /var/spool/cron/root
           sed -i "/acme.sh/c 0 3 * * 0 bash ${ssl_update_file}" /var/spool/cron/root
